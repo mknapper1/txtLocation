ade8f61bb5fa53eb292b2e54155d62a5cfe13d68
Matt Knapper <mknapper1@gmail.com>
2018-04-21 12:07:58 -0400

    boto settings

--- location/models.py
+++ location/models.py
@@ -36,7 +36,7 @@ class Location(models.Model):
             "sns",
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            # region_name="us-east-1"
+            region_name="us-east-1"
         )
 
         client.publish(
